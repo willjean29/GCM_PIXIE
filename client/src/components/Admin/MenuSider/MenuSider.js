6f966252cfcf7bf87f4a5ed7996a77552f2b30a2
Jean Osco <willjean29@gmail.com>
2020-11-13 18:01:50 -0500

    PIXIE-027 Ajustes del sidebar del administrador

--- client/src/components/Admin/MenuSider/MenuSider.js
+++ client/src/components/Admin/MenuSider/MenuSider.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Link, useHistory} from 'react-router-dom';
-import {Layout, Menu} from 'antd';
+import {useSelector} from 'react-redux';
+import {Layout, Menu, Tooltip} from 'antd';
 import {
   HomeOutlined, 
   UserOutlined, 
@@ -16,6 +17,8 @@ const MenuSider = (props) => {
   const {location:{pathname}} = useHistory();
   const {SubMenu} = Menu;
   const {Sider} = Layout;
+  const administrador = useSelector(state => state.authentication.user);
+  console.log(administrador);
   return (  
     <Sider className="admin-sider" collapsed={menuCollapsed}>
       <Menu theme="dark" mode="inline" defaultSelectedKeys={[pathname]}>
@@ -32,38 +35,67 @@ const MenuSider = (props) => {
           </Link>
         </Menu.Item>
         <SubMenu icon={<BankOutlined />} title="Empresa">
-          <Menu.Item key="/admin/business/new">
-            <Link to="/admin/business/new">
-              <CarryOutOutlined />
-              <span className="nav-text">Registrar</span>
-            </Link>
-          </Menu.Item>
-          <Menu.Item key="/admin/business/info">
-            <Link to="/admin/business/info">
-              <CarryOutOutlined />
-              <span className="nav-text">Información</span>
-            </Link>
-          </Menu.Item>
+          {
+            administrador && administrador.estado ? (
+              <Menu.Item key="/admin/business/info">
+                <Link to="/admin/business/info">
+                  <CarryOutOutlined />
+                  <span className="nav-text">Información</span>
+                </Link>
+              </Menu.Item>
+            ) : (
+              <Menu.Item key="/admin/business/new">
+                <Link to="/admin/business/new">
+                  <CarryOutOutlined />
+                  <span className="nav-text">Registrar</span>
+                </Link>
+              </Menu.Item>
+            )
+          }
         </SubMenu>
-        <SubMenu icon={<AimOutlined />} title="Concursos">
-          <Menu.Item key="/admin/competition/new">
-            <Link to="/admin/competition/new">
-              <CarryOutOutlined />
-              <span className="nav-text">Registrar</span>
-            </Link>
-          </Menu.Item>
-          <Menu.Item key="/admin/competition/info">
-            <Link to="/admin/competition/info">
-              <CarryOutOutlined />
-              <span className="nav-text">Información</span>
-            </Link>
-          </Menu.Item>
-        </SubMenu>
+        {
+          administrador && administrador.estado ? (
+            <SubMenu icon={<AimOutlined />} title="Concursos">
+              <Menu.Item key="/admin/competition/new">
+                <Link to="/admin/competition/new">
+                  <CarryOutOutlined />
+                  <span className="nav-text">Registrar</span>
+                </Link>
+              </Menu.Item>
+              <Menu.Item key="/admin/competition/info">
+                <Link to="/admin/competition/info">
+                  <CarryOutOutlined />
+                  <span className="nav-text">Información</span>
+                </Link>
+              </Menu.Item>
+            </SubMenu>
+          ) : (
+            <Menu.Item key="/admin/competition">
+              <Tooltip title="Registre una empresa para habilitar esta opción" placement="top" arrowPointAtCenter color={'#108ee9'}>
+                <div>
+                  <AimOutlined />
+                  <span className="nav-text">Concursos</span>
+                </div>
+              </Tooltip>
+            </Menu.Item>
+          )
+        }
         <Menu.Item key="/admin/files">
-          <Link to="/admin/files">
-            <FileExcelOutlined />
-            <span className="nav-text">Archivos</span>
-          </Link>
+          {
+            administrador && administrador.estado ? (
+              <Link to="/admin/files">
+                <FileExcelOutlined />
+                <span className="nav-text">Archivos</span>
+              </Link>
+            ) : (
+              <Tooltip title="Registre una empresa para habilitar esta opción" placement="top" arrowPointAtCenter color={'#108ee9'} defaultVisible={true}>
+                <div>
+                  <FileExcelOutlined />
+                  <span className="nav-text">Archivos</span>
+                </div>
+              </Tooltip>
+            )
+          }
         </Menu.Item>
       </Menu>
     </Sider>
