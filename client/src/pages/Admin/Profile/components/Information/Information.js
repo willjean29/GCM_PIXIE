b253c9f1c27da7d393aaad28530e184ea4f627f9
Jean Osco <willjean29@gmail.com>
2020-11-27 16:39:42 -0500

    PIXIE-032 Interfaz para editar perfil de administrador

---
+++ client/src/pages/Admin/Profile/components/Information/Information.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import {Card, Table} from 'antd';
+import { RedditOutlined, InfoCircleOutlined, CheckCircleOutlined} from '@ant-design/icons';
+const Information = ({administrator}) => {
+  const columns = [
+    {
+      title: 'Dato',
+      dataIndex: 'dato',
+      key: 'dato',
+      render: (text) => (
+        <span style={{fontWeight: 'bold'}}>{text}</span>
+      )
+    },
+    {
+      title: 'Carácteristica',
+      dataIndex: 'caracteristica',
+      key: 'caracteristica',
+    }
+  ]
+
+  const data = [
+    {
+      key: '1',
+      dato: 'DNI',
+      caracteristica: administrator && administrator.dni
+    },
+    {
+      key: '2',
+      dato: 'Nombres',
+      caracteristica: administrator && administrator.names
+    },
+    {
+      key: '3',
+      dato: 'Apellido Paterno',
+      caracteristica: administrator && administrator.lastNameP
+    },
+    {
+      key: '4',
+      dato: 'Apellido Materno',
+      caracteristica:  administrator && administrator.lastNameA
+    },
+    {
+      key: '5',
+      dato: 'Fecha de Nacimiento',
+      caracteristica: administrator && (administrator.fechaNacimiento ? 
+        administrator.fechaNacimiento : "DD/MM/YYYY")
+    },
+    {
+      key: '6',
+      dato: 'Telefono',
+      caracteristica:  administrator && (administrator.celular ? 
+        administrator.celular : "----------")
+    },
+    {
+      key: '7',
+      dato: 'Correo',
+      caracteristica: administrator && administrator.email
+    },
+    {
+      key: '8',
+      dato: 'Direccion',
+      caracteristica: administrator && (administrator.direccion ? 
+        administrator.direccion : "----------")
+    },
+    {
+      key: '9',
+      dato: 'Departamento',
+      caracteristica: administrator && (administrator.departamento ? 
+        administrator.departamento : "----------")
+    },
+    {
+      key: '10',
+      dato: 'Provincia',
+      caracteristica: administrator && (administrator.provincia ? 
+        administrator.provincia : "----------")
+    },
+    {
+      key: '11',
+      dato: 'Distrito',
+      caracteristica: administrator && (administrator.distrito ? 
+        administrator.distrito : "----------")
+    },
+  ]
+  return (  
+    <Card>
+      <Table 
+        columns={columns} 
+        dataSource={data}
+        pagination={false}
+        bordered
+        className="table-competition"
+      />
+    </Card>
+  );
+}
+ 
+export default Information;
