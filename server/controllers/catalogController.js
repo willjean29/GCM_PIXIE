3022bed6f09edca9a2c68c4f58c8509b33059f3e
Stephanie <stephanie.azorsa@unmsm.edu.pe>
2020-12-01 02:35:49 -0500

    PIXIE-037 Buscar los premios asociados a la empresa

--- server/controllers/catalogController.js
+++ server/controllers/catalogController.js
@@ -1,19 +1,45 @@
-const Administrator = require('../models/Administrator');
+const Catalog = require("../models/Catalog");
+const Administrator = require("../models/Administrator");
+const Prize = require("../models/Prize");
+const Business = require("../models/Business");
+const Category = require("../models/Category");
 
-const {existsCompetitionSimple,existsCatalogoBusiness} = require('../middlewares/exists');
+const {
+  existsCompetitionSimple,
+  existsCatalogoBusiness,
+} = require("../middlewares/exists");
 
-const mostrarCrearCatalogo = async(req,res) => {
+const mostrarCrearCatalogo = async (req, res) => {
   const administrator = await Administrator.findById(req.user._id).lean();
   const existeConcursoSimple = await existsCompetitionSimple(req.user._id);
   const existeCatalogoBusiness = await existsCatalogoBusiness(req.user._id);
   res.send({
-    title: 'Adminstrador',
+    title: "Adminstrador",
     admin: administrator,
     existeConcursoSimple,
-    existeCatalogoBusiness
+    existeCatalogoBusiness,
   });
-}
+};
+
+const mostrarListaCatalogo = async (req, res) => {
+  const administrator = await Administrator.findById(req.user._id).lean();
+  const business = await Business.findOne({
+    administrador: administrator._id,
+  }).lean();
+  const catalog = await Catalog.findOne({ business: business._id }).lean();
+  const prizes = await Prize.find({ catalog: catalog._id })
+    .populate("category", "name")
+    .lean();
+  const existeConcursoSimple = await existsCompetitionSimple(req.user._id);
+  const existeCatalogoBusiness = await existsCatalogoBusiness(req.user._id);
+  const categories = await Category.find().sort("name").lean();
+
+  // console.log(categories);
+  // console.log(existeCatalogoBusiness);
+  // console.log(prizes)
+};
 
 module.exports = {
   mostrarCrearCatalogo,
-}
+  mostrarListaCatalogo,
+};
