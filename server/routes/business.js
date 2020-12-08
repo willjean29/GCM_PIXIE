f6ebfffd276437f25d2290ebffa024b19da1b59e
Stephanie <stephanie.azorsa@unmsm.edu.pe>
2020-12-08 02:33:05 -0500

    PIXIE-029 Se agregó la ruta de Actualizar ubicación de empresa

--- server/routes/business.js
+++ server/routes/business.js
@@ -1,45 +1,50 @@
 /**
  * Rutas de la empresa
  */
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Importando controladores
-const businessController = require('../controllers/businessController');
-const authController = require('../controllers/authController');
+const businessController = require("../controllers/businessController");
+const authController = require("../controllers/authController");
 
-const { verifyTokenAdmin } = require('../middlewares/verifyToken');
-const { uploadImage } = require('../middlewares/uploadImage');
+const { verifyTokenAdmin } = require("../middlewares/verifyToken");
+const { uploadImage } = require("../middlewares/uploadImage");
 
-router.get('/',
-  verifyTokenAdmin,
-  businessController.obtenerEmpresa
-);
+// Para obtener información de la empresa
+router.get("/", verifyTokenAdmin, businessController.obtenerEmpresa);
 
 // Para validar RUC
-router.post('/verificar-ruc',
+router.post(
+  "/verificar-ruc",
   // authController.adminsitradorAutenticado,
   verifyTokenAdmin,
   businessController.validarRUC
 );
 
 // Para registar empresa
-router.post('/registrar',
+router.post(
+  "/registrar",
   verifyTokenAdmin,
   businessController.registrarEmpresa
 );
 
 // actualizar empresa
-router.put('/',
-  verifyTokenAdmin,
-  businessController.actualizarEmpresa
-);
+router.put("/", verifyTokenAdmin, businessController.actualizarEmpresa);
 
 // agregar/actualizar avatar de la empresa
-router.put('/avatar',
+router.put(
+  "/avatar",
   verifyTokenAdmin,
   uploadImage,
   businessController.agregarAvatarEmpresa
+);
+
+// actualizar ubicación de empresa
+router.put(
+  "/ubicacion",
+  verifyTokenAdmin,
+  businessController.actualizarUbicacionEmpresa
 );
 
 module.exports = router;
