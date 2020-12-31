48d68086c9bbbec30884300bf0cdef29a16ec6f3
Stephanie <stephanie.azorsa@unmsm.edu.pe>
2020-12-31 03:51:58 -0500

    PIXIE-034 mostrarCategoriaCatalogo agregado

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,8 @@
 /**
  * USERCONTROLLER
- * Controlador del Cliente, controla las operaciones de registro, 
- * login, mostrar lista de empresas, mostrar catálogo de empresa...
+ * Controlador del Cliente, controla las operaciones de registro,
+ * login, mostrar listado de empresas, mostrar catálogo de empresa,
+ * filtros de búsqueda...
  */
 
 const Client = require("../models/Client");
@@ -201,10 +202,109 @@ const mostrarCatalogoEmpresa = async (req, res) => {
   });
 };
 
+const mostrarCategoriaCatalogo = async (req, res) => {
+  const categorias = await obtenerCategorias(req.params.id);
+  const catalogo = await Catalog.findOne({ business: req.params.id });
+  const category = await Category.findOne({ name: req.params.category });
+
+  // premios por pagina
+  let premio = 6;
+  let paginaActual = req.query.p || 1;
+
+  const filtroPuntos = req.query.puntos || false;
+  // condicionar la busqueda de premiosTotales
+  let premios;
+  let premiosTotales;
+  if (filtroPuntos) {
+    premios = await Prize.find({
+      catalog: catalogo._id,
+      category: category._id,
+      points: { $gte: 0, $lte: filtroPuntos },
+    })
+      .skip(premio * paginaActual - premio)
+      .limit(premio)
+      .sort("points")
+      .lean();
+    console.log(premios);
+    premiosTotales = await Prize.count({
+      catalog: catalogo._id,
+      category: category._id,
+      points: { $gte: 0, $lte: filtroPuntos },
+    });
+  } else {
+    premios = await Prize.find({
+      catalog: catalogo._id,
+      category: category._id,
+    })
+      .skip(premio * paginaActual - premio)
+      .limit(premio)
+      .sort("points")
+      .lean();
+    console.log(premios);
+    premiosTotales = await Prize.count({
+      catalog: catalogo._id,
+      category: category._id,
+    });
+  }
+
+  const paginasTotales = Math.ceil(premiosTotales / premio);
+
+  // obtener datos de la empresa actual
+  //  cargar las empresas asosciadas
+  let afiliadas = await actualizarEmpresasAfiliadas(req);
+  let empresaActual;
+  for (let empresa of afiliadas) {
+    if (req.params.id == empresa.id) {
+      empresaActual = empresa;
+    }
+  }
+
+  res.send({
+    layout: "user.hbs",
+    categorias,
+    premios,
+    pagination: {
+      page: paginaActual, // The current page the user is on
+      pageCount: paginasTotales, // The total number of available pages
+      parmPuntos: filtroPuntos,
+    },
+    empresa: empresaActual,
+    name: req.user.name,
+    Session: true,
+    filtroPuntos,
+  });
+};
+
+const obtenerCategorias = async (idBusiness) => {
+  const catalogo = await Catalog.findOne({ business: idBusiness });
+  const premios = await Prize.find({ catalog: catalogo._id });
+
+  let categorias = [];
+  let categories = new Set();
+
+  for (let premio of premios) {
+    const category = await Category.findById(premio.category);
+    categories.add(category.name);
+  }
+
+  categories = Array.from(categories).sort();
+  for (let category of categories) {
+    let data;
+    data = {
+      name: category,
+      empresa: idBusiness,
+      catalogo: catalogo._id,
+    };
+    categorias.push(data);
+  }
+
+  return categorias;
+};
+
 module.exports = {
   registrarCliente,
   autenticarCliente,
   mostrarListadoEmpresas,
-  actualizarEmpresasAfiliadas,
   mostrarCatalogoEmpresa,
+  mostrarCategoriaCatalogo,
 };
