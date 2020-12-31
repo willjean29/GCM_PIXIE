a032238d2eebb22c63dda0ac7b08f5b8f5526236
Stephanie <stephanie.azorsa@unmsm.edu.pe>
2020-12-31 03:33:16 -0500

    PIXIE-003 Endpoint de LogIn agregado

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,11 @@
-/*
-  Agrupa archivos, en este caso las rutas 
-  para exportarlas en una solo.
-*/
+/**
+ * Agrupa archivos, en este caso las rutas 
+ * para exportarlas en una solo.
+ */
 
 // Importando librerías
 const express = require('express');
 const app = express();
-
-// app.use(require)
 
 // endpoints for admin
 app.use("/admin", require("./admin"));
@@ -29,5 +27,8 @@ app.use('/catalog',require('./catalog'));
 
 // endpoints for prizes
 app.use('/prize',require('./prize'));
+
+// endpoints for activities users
+app.use('/',require('./user'));
 
 module.exports = app;
