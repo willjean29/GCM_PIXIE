ae13852788b5a7a823b1114850caa5600a3b1f41
Stephanie <stephanie.azorsa@unmsm.edu.pe>
2020-12-31 03:17:07 -0500

    PIXIE-002 Endpoint agregado

---
+++ server/routes/user.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express.Router();
+const userController = require("../controllers/userController");
+
+const { verifyTokenCliente } = require("../middlewares/verifyToken");
+
+app.post("/login", userController.autenticarCliente);
+
+app.post("/register", userController.registrarCliente);
+
+app.get("/business_list", 
+  verifyTokenCliente, 
+  userController.mostrarListadoEmpresas
+);
+
+app.get(
+  "/business/:id",
+  verifyTokenCliente,
+  userController.mostrarCatalogoEmpresa
+);
+
+app.get(
+  "/business/:id/:category",
+  verifyTokenCliente,
+  userController.mostrarCategoriaCatalogo
+);
+
+module.exports = app;
